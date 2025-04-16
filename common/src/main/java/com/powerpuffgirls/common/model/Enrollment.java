73758fba3a403f5f01bf51d35fbad038d01d7d75
Jomar Delos Reyes <jomardelosreyes32@gmail.com>
2025-04-16 23:24:21 +0800

    refactor: add enrollment class in common

---
+++ common/src/main/java/com/powerpuffgirls/common/model/Enrollment.java
@@ -0,0 +1,50 @@
+package com.powerpuffgirls.common.model;
+
+import jakarta.persistence.*;
+
+@Entity // Map to the existing student_course table
+public class Enrollment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "student_id", nullable = false)
+    private int studentId;
+
+    @Column(name = "course_id", nullable = false)
+    private int courseId;
+
+
+    public Enrollment() {
+    }
+
+    public Enrollment(int studentId, int courseId) {
+        this.studentId = studentId;
+        this.courseId = courseId;
+    }
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    public int getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(int courseId) {
+        this.courseId = courseId;
+    }
+}
